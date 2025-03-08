704853e3d97126a512ac46f035e2c35fa4749f36
muftira <muh.muftir@gmail.com>
2025-03-08 12:26:09 +0700

    add validation for multer and cloudinary

--- midlewares/multer.js
+++ midlewares/multer.js
@@ -20,7 +20,8 @@ const storage = new CloudinaryStorage({
 const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'image/jpeg' ||
-    file.mimetype === 'image/png'
+    file.mimetype === 'image/png' || 
+    file.mimetype === 'image/jpg'
   ) {
     cb(null, true);
   } else {
